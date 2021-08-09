99131e59520858bd0b22349d4aeb9cbe8360ffe2
Piotr Graczyk <goldlightdrake@gmail.com>
2021-08-09 21:23:28 +0200

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBULhLFqLFefe055cuzPsrIcXp0mqX0NqA",
+  authDomain: "refcue-1793c.firebaseapp.com",
+  databaseURL: "https://refcue-1793c.firebaseio.com",
+  projectId: "refcue-1793c",
+  storageBucket: "refcue-1793c.appspot.com",
+  messagingSenderId: "632003054989",
+  appId: "1:632003054989:web:4e259b8381fcca317981c4",
+  measurementId: "G-DMRVZ2T3P3",
+};
+
+export default firebaseConfig;
